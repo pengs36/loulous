c853f5bb83aa7b0f4e19d7bfdd57f92aff088920
pings <patricksaliba36@gmail.com>
2022-11-15 15:49:43 +0200

    fixed javascript

--- project loulz/louloujs.js
+++ project loulz/louloujs.js
@@ -8,40 +8,6 @@ function hidecigs() {
   document.getElementById("cigs").style.display = "none";
 }
 let selectBox = document.getElementById("dd1");
-let val=selectBox.value;
-function hideshow(val){
-        if(val==='1'){
-        document.getElementById('snacks').style.display='flex'; 
-        document.getElementById('drinks').style.display='flex';
-        document.getElementById('cigs').style.display='flex';
-        }
-        else if(val==='2'){
-        hidedrinks();
-        hidecigs();
-        document.getElementById('snacks').style.display='flex';
-        }
-        else if(val==='3'){
-        hidesnacks();
-        hidecigs();
-        document.getElementById('drinks').style.display='flex';
-        }
-        else if(val==='4'){
-        hidedrinks();
-        hidesnacks();
-        document.getElementById('cigs').style.display='flex';
-        }
-    }
-        function openForm() {
-            document.getElementById("myForm").style.display = "block";
-          }
-          
-        function closeForm() {
-            document.getElementById("myForm").style.display = "none";
-        }
-        function sub(event){
-        var em=document.getElementById("email").value.trim();
-        var categ=document.getElementById("category").value.trim();
-        var prod=document.getElementById("product").value.trim();
 let val = selectBox.value;
 function hideshow(val) {
   if (val === "1") {
@@ -69,6 +35,7 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
 function sub(event) {
   var em = document.getElementById("email").value.trim();
   var categ = document.getElementById("category").value.trim();
@@ -86,9 +53,8 @@ function sub(event) {
     (emLength == null || emLength == "",
     categLength == null || categLength == "",
     prodLength == null || prodLength == "")
-  ) 
+  )
     event.preventDefault();
-    alert("please fill all fields");
-    return false;
+  alert("please fill all fields");
+  return false;
 }
-        }
